89becbc90c3a550e53d67dd7ac380022ca6770f1
Jufebrown <jufebrown@gmail.com>
2017-02-01 10:11:49 -0600

    started functions for checking for victory conditions

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const config = {
-    apiKey: "AIzaSyBw6ShnJKM2K59nC-x19TSBGsdd94KZGz0",
-    authDomain: "tic-tac-toe-firebase.firebaseapp.com",
-    databaseURL: "https://tic-tac-toe-firebase.firebaseio.com",
-    storageBucket: "tic-tac-toe-firebase.appspot.com",
-    messagingSenderId: "315055648875"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBw6ShnJKM2K59nC-x19TSBGsdd94KZGz0",
+  authDomain: "tic-tac-toe-firebase.firebaseapp.com",
+  databaseURL: "https://tic-tac-toe-firebase.firebaseio.com",
+  storageBucket: "tic-tac-toe-firebase.appspot.com",
+  messagingSenderId: "315055648875"
+};
 
+firebase.initializeApp(config);
+
+//varible declarations
+const rows = []
+const cols = []
+
+
+//functions
+function checkRows() {
+  for(var i = 0, length1 = rows.length; i < length1; i++){
+    rows[i]
+  }
+}
+
+function winCheck() {
+  checkRows()
+  checkCols()
+  checkDiags()
+}
+
+//event listeners
 $('.square').click(function() {
 	console.log(this.dataset)
 })
