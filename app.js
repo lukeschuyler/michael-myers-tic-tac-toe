fdaf30a42baec7ed1a5bd1264c214e0ab21f573c
Luke Schuyler <lukeschuyler1@gmail.com>
2017-02-01 15:12:11 -0600

    git happy

--- app.js
+++ app.js
@@ -10,58 +10,84 @@ const config = {
 let currentGame;
 let currentSeatOne;
 let currentSeatTwo;
-let turnsRef = firebase.database().ref('games/' + currentGame + '/turns')
+let turnsRef = firebase.database().ref('games/turns')
 const gamesRef = firebase.database().ref('games')
 let userRef = firebase.database().ref('games/' + currentGame + '/users')
 
-$('.square').click(makeMove)
-
-$('.new-game').click(newGame)
-
-let i = 0;
-
-function makeMove() {
-	if (this.innerHTML === '') {
-		let square = $(this).attr('id')
-		let turn;
-		if ((i % 2) === 0 || i === 0) {
-			turn = '<span class="letter">X</span>'
-			$(this).html(turn)
-			i++
-			return turnsRef.update({ [square] : 'X' })
-		} else {
-			turn = '<span class="letter">O</span>'
-			$(this).html(turn)
-			i++
-			return turnsRef.update({ [square] : 'O' })
-		}
+function cheackSeats() {
+	if (document.querySelector('.seatOne').innerHTML && document.querySelector('.seatTwo').innerHTML === 'Seat Taken') {
+		userRef.update({ twoplayers : true, [currentSeatOne] : 'X', [currentSeatTwo] : 'O' })
 	}
 }
 
-
-function newGame() {
-	let removeGame = { [currentGame] : null }
-	gamesRef.update(removeGame)
-	const newBoard = { turns : 'test' }
-	document.querySelectorAll('.square').forEach(function(square) {
-		square.innerText = null
-	})
-	gamesRef.push(newBoard)
-	  .then(data => currentGame = data.path.o[1])
-	  .then(() => {
-	  	turnsRef = firebase.database().ref('games/' + currentGame + '/turns')
-	  	userRef = firebase.database().ref('games/' + currentGame + '/users')
-	})
-	i = 0;
+function resetSeats() {
+	$('.seatOne').html('Join Seat 1 (X)')
+	$('.seatTwo').html('Join Seat 2 (O)')
+	$('.seatOne').removeClass('btn-primary')
+	$('.seatTwo').removeClass('btn-primary')
+	$('.seatOne').addClass('btn-default')
+	$('.seatTwo').addClass('btn-default')
 }
 
 $('.seat').click(function(e) {
 	$(this).html('Seat Taken')
 	$(this).removeClass('btn-default')
 	$(this).addClass('btn-primary')
-	if (document.querySelector('.seatOne').innerHTML === 'Seat Taken' && document.querySelector('.seatTwo').innerHTML === 'Seat Taken') {
-		userRef.update({twoplayers : true})
+	console.log($(this).html('Seat Taken'))
+	if ($(this).hasClass('seatOne') === true) {
+		firebase.auth().signInAnonymously()
+			.then(val => currentSeatOne = val.uid)
+			.then(function() {
+				cheackSeats()
+			})
+	} else if ($(this).hasClass('seatTwo') === true){
+		firebase.auth().signInAnonymously()
+			.then(val => currentSeatTwo = val.uid)
+			.then(function() {
+				cheackSeats()
+			})
 	}
 })
 
-newGame();
+function newGame() {
+		let removeGame = { [currentGame] : null }
+		gamesRef.update(removeGame)
+		const newBoard = { turns : 'test' }
+		document.querySelectorAll('.square').forEach(function(square) {
+			square.innerText = null
+		})
+		gamesRef.push(newBoard)
+		  .then(data => currentGame = data.path.o[1])
+		  .then(() => {
+		  	turnsRef = firebase.database().ref('games/' + currentGame + '/turns')
+		  	userRef = firebase.database().ref('games/' + currentGame + '/users')
+		})
+		i = 0;
+		resetSeats()
+	}
+
+	let i = 0
+
+	function makeMove() {
+		if (this.innerHTML === '') {
+			let square = $(this).attr('id')
+			let turn;
+			if ((i % 2) === 0 || i === 0) {
+				turn = '<span class="letter">X</span>'
+				$(this).html(turn)
+				i++
+				return turnsRef.update({ [square] : 'X' })
+			} else {
+				turn = '<span class="letter">O</span>'
+				$(this).html(turn)
+				i++
+				return turnsRef.update({ [square] : 'O' })
+			}
+		}
+	}
+
+	newGame();
+
+	$('.square').click(makeMove)
+
+	$('.new-game').click(newGame)
