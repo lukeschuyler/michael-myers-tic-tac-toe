7bd5ec5b47ce0278c95519d3d27a042ca0025af4
Jufebrown <jufebrown@gmail.com>
2017-02-01 13:06:50 -0600

    organized code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const config = {
   apiKey: "AIzaSyBw6ShnJKM2K59nC-x19TSBGsdd94KZGz0",
   authDomain: "tic-tac-toe-firebase.firebaseapp.com",
@@ -8,20 +9,27 @@ const config = {
 
 firebase.initializeApp(config);
 
+/*********************************
+variable declarations
+*********************************/
+
 let currentGame;
 let turnsRef = firebase.database().ref('games/' + currentGame + '/turns')
 const gamesRef = firebase.database().ref('games')
 let userRef = firebase.database().ref('games/' + currentGame + '/users')
+let i = 0
+
+
 
 /*********************************
 functions
 *********************************/
 
-
-
 function onUpdate(snap) {
+  console.log('onUpdate')
+  console.log(snap)
   const data = snap.val()
-  console.log(snap.val)
+  console.log(data)
   // winCheck()
 }
 
@@ -30,50 +38,51 @@ function onUpdate(snap) {
 // }
 
 
+function makeMove() {
+  // console.log(i)
+  let square = $(this).attr('id')
+  let turn;
+  if ((i % 2) === 0 || i === 0) {
+    turn = '<span class="letter">X</span>'
+    $(this).html(turn)
+    i++
+    return turnsRef.update({ [square] : 'X' })
+  } else {
+    turn = '<span class="letter">O</span>'
+    $(this).html(turn)
+    i++
+    return turnsRef.update({ [square] : 'O' })
+  }
+}
+
+function newGame() {
+  const newBoard = { turns : 'test' }
+  document.querySelectorAll('.square').forEach(function(square) {
+    square.innerText = null
+  })
+  gamesRef.push(newBoard)
+    .then(data => currentGame = data.path.o[1])
+    .then(() => turnsRef = firebase.database().ref('games/' + currentGame + '/turns'))
+  i = 0;
+}
+
 
 /*********************************
 event listeners
 *********************************/
-$('.square').click(function() {
-  console.log(this.dataset)
-})
 
-firebase.database().ref('turns').on('value', onUpdate)
-
-
-
-
+// user clicks on square
 $('.square').click(makeMove)
 
+// user clicks new game button
 $('.new-game').click(newGame)
 
-	let i = 0
-	function makeMove() {
-		console.log(i)
-		let square = $(this).attr('id')
-		let turn;
-		if ((i % 2) === 0 || i === 0) {
-			turn = '<span class="letter">X</span>'
-			$(this).html(turn)
-      i++
-			return turnsRef.update({ [square] : 'X' })
-		} else {
-			turn = '<span class="letter">O</span>'
-			$(this).html(turn)
-		  i++
-      return turnsRef.update({ [square] : 'O' })
-    }
-	}
+gamesRef.on('child_changed', onUpdate)
 
-function newGame() {
-	const newBoard = { turns : 'test' }
-	document.querySelectorAll('.square').forEach(function(square) {
-		square.innerText = null
-	})
-	gamesRef.push(newBoard)
-	  .then(data => currentGame = data.path.o[1])
-	  .then(() => turnsRef = firebase.database().ref('games/' + currentGame + '/turns'))
-	i = 0;
-}
+
+
+/*********************************
+starts a new game as soon as app starts
+*********************************/
 
 newGame();
